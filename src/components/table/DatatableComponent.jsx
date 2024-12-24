2c50272ddceb0daadc925cfdaf063f69d15c0f74
Poovarasan <poosan9@gmail.com>
2024-12-25 00:10:59 +0530

    datagrid added

--- src/components/table/DatatableComponent.jsx
+++ src/components/table/DatatableComponent.jsx
@@ -2,39 +2,42 @@ import { AgGridReact } from "ag-grid-react";
 import Box from "@mui/joy/Box";
 import { themeQuartz } from 'ag-grid-community'
 import { useTheme } from "@mui/joy";
+import { useMemo } from "react";
 
 
 
 export const DatatableComponent = ({ rows, columns }) => {
 
   const theme = useTheme();
-  const myTheme = themeQuartz
-    .withParams({
-      backgroundColor: theme.palette.background.body,
-      borderColor: theme.palette.divider,
-      borderRadius: 10,
+  const myTheme = useMemo(() => {
+    return themeQuartz
+      .withParams({
+        backgroundColor: theme.palette.background.body,
+        borderColor: theme.palette.divider,
+        borderRadius: 10,
 
-      browserColorScheme: "light",
-      cellHorizontalPaddingScale: 0.7,
-      chromeBackgroundColor: {
-        ref: "backgroundColor"
-      },
-      columnBorder: false,
-      fontFamily: "inherit",
-      fontSize: 13,
-      foregroundColor: theme.palette.text.primary,
-      headerBackgroundColor: theme.palette.background.level1,
-      headerFontSize: 13,
-      headerFontWeight: 600,
-      headerTextColor: theme.palette.text.primary,
-      rowBorder: true,
-      rowVerticalPaddingScale: 0.8,
-      sidePanelBorder: true,
-      spacing: 6,
-      wrapperBorder: true,
-      wrapperBorderRadius: 10,
-      headerHeight: 32,
-    });
+        browserColorScheme: "light",
+        cellHorizontalPaddingScale: 0.7,
+        chromeBackgroundColor: {
+          ref: "backgroundColor"
+        },
+        columnBorder: false,
+        fontFamily: "inherit",
+        fontSize: 13,
+        foregroundColor: theme.palette.text.primary,
+        headerBackgroundColor: theme.palette.background.level1,
+        headerFontSize: 13,
+        headerFontWeight: 600,
+        headerTextColor: theme.palette.text.primary,
+        rowBorder: true,
+        rowVerticalPaddingScale: 0.8,
+        sidePanelBorder: true,
+        spacing: 6,
+        wrapperBorder: true,
+        wrapperBorderRadius: 10,
+        headerHeight: 32,
+      });
+  },[ theme ])
 
   return (
     <Box sx={{width: '100%', pb: 2}}>
